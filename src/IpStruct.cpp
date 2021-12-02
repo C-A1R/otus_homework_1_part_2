262db178cda495fa50c4d14247ac02a19e0e5890
C-A1R <al.korsakov@list.ru>
2021-12-03 02:37:15 +0300

    Revert "remove all files for review"
    This reverts commit 87f7be6e8675742de056e8d221e70a259e2e5106.

---
+++ src/IpStruct.cpp
@@ -0,0 +1,54 @@
+#include "IpStruct.h"
+
+#include <iostream>
+
+IpStruct::IpStruct(const std::vector<std::string> &v)
+{
+    if (v.size() != ipVersion)
+    {
+        return;
+    }
+
+    bytes.reserve(ipVersion);
+    for (const auto &byte_str : v)
+    {
+        int byte = std::stoi(byte_str);
+        if (byte < 0 || byte > 255)
+        {
+            bytes.clear();
+            break;
+        }
+        bytes.emplace_back(byte);
+    }
+}
+
+int IpStruct::getByte(const int index) const
+{
+    if (!isValid())
+    {
+        return -1;
+    }
+    if (index >= 0 && index < ipVersion)
+    {
+        return bytes[index];
+    }
+    return -1;
+}
+
+void IpStruct::print() const
+{
+    for (auto iter = bytes.cbegin(); iter != bytes.cend(); ++iter)
+    {
+        if (iter != bytes.cbegin())
+        {
+            std::cout << '.';
+        }
+        std::cout << *iter;
+    }
+    std::cout << std::endl;
+}
+
+bool IpStruct::isValid() const
+{
+    return (!bytes.empty());
+}
